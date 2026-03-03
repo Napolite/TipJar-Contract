422a29862fcb832f2a59c9009d2212c0e90704d1
Constantine <Okonemmanuelmma@gmail.com>
2026-03-03 13:20:10 +0100

    define struct for senders

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,6 +4,28 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Vault{
-    uint public 
+contract TipContract{
+    
+    struct Token{
+    string name;
+    string ticker;
+    string amount;
+  }
+  
+  struct Tip{
+    address sender;
+    uint amount;
+    string message;
+    Token token;
+  }
+
+  struct TipJar{
+    uint balance;
+    address payable owner;
+    string ownerName;
+    uint date;
+  }
+
+   mapping(address addr=>  Tip ) public senders,
+  
 }
