b8c6d71289e74aed095565244ddc1b5a1222a510
Constantine <Okonemmanuelmma@gmail.com>
2026-03-04 12:57:34 +0100

    extension

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -4,28 +4,31 @@ pragma solidity ^0.8.28;
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract TipContract{
-    
-    struct Token{
-    string name;
-    string ticker;
-    string amount;
-  }
-  
-  struct Tip{
-    address sender;
-    uint amount;
-    string message;
-    Token token;
-  }
+contract TipContract {
+    struct Token {
+        string name;
+        string ticker;
+        string amount;
+    }
 
-  struct TipJar{
-    uint balance;
-    address payable owner;
-    string ownerName;
-    uint date;
-  }
+    struct Tip {
+        address sender;
+        uint amount;
+        string message;
+        Token token;
+        uint data;
+    }
 
-   mapping(address addr=>  Tip ) public senders,
-  
+    struct TipJar {
+        uint balance;
+        address payable owner;
+        string ownerName;
+        uint date;
+        Tip[] tips;
+    }
+
+    mapping(address addr => Tip) public senders;
+    mapping(address owner => TipJar) public tipJars;
+
+    function createTipJar(address owner, string memory ownerName) public {}
 }
